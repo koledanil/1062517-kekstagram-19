dcdba80692da8e02d2fd0cd7ac12cf0283b04ef6
koledanil <koledanil@gmail.com>
2020-03-09 12:52:39 +0200

    change interval

--- js/upload-img/title-error.js
+++ js/upload-img/title-error.js
@@ -55,7 +55,7 @@
         document.title = secondTxt;
       }
     };
-    intervalId = setInterval(switchTitle, 1100);
+    intervalId = setInterval(switchTitle, 600);
     return intervalId;
   };
 
